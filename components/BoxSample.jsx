e838691375da7c813a271dcf3ae041af642c1a90
Jude <judeputhussery360@gmail.com>
2023-12-29 21:29:00 +0530

    added boxes and rotation

---
+++ components/BoxSample.jsx
@@ -0,0 +1,22 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
+
+export const BoxSample = ({ position, size, color }) => {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta * 0.5;
+    ref.current.rotation.y += delta;
+    ref.current.rotation.z = Math.sin(state.clock.elapsedTime) * 2;
+  });
+  return (
+    <>
+      <OrbitControls />
+      <directionalLight position={[0, 0, 2]} intensity={0.7} />
+      <mesh position={position} ref={ref}>
+        <boxGeometry args={size} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    </>
+  );
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { Canvas } from '@react-three/fiber'
-import './App.css'
-import { Experience } from '../components/Experience'
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
+import { BoxSample } from "../components/BoxSample";
 
 function App() {
-
   return (
     <Canvas>
-      <Experience />
+      <ambientLight intensity={0.5} />
+      <group position={[-0, 0, 0]}>
+        <BoxSample position={[1, 0, 0]} size={[1, 1, 1]} color="red" />
+        <BoxSample position={[-1, 0, 0]} size={[1, 1, 1]} color="blue" />
+        <BoxSample position={[0, 1, 0]} size={[1, 1, 1]} color="green" />
+        <BoxSample position={[0, -1, 0]} size={[1, 1, 1]} color="yellow" />
+      </group>
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
