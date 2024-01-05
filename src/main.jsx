582ab5a6b0aaedd9bcbe156ce9a688fda4abe713
Jude <judeputhussery360@gmail.com>
2024-01-06 01:24:57 +0530

    Dark theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme appearance="inherit" accentColor="purple">
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
