bc9d044954f7aa3a4f04c2813d21c0f0bb2041f1
Jude <judeputhussery360@gmail.com>
2024-01-06 20:37:14 +0530

    added BMO

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,17 +1,25 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import NavBar from "./NavBar";
 import { GdscLogo } from "../../components/GdscLogo";
+import { Bmo } from "../../components/Bmo";
+import { OrbitControls } from "@react-three/drei";
 const Header = () => {
     return (
         <>
         <NavBar />
         <Canvas>
-            <OrbitControls />
             <ambientLight intensity={2} />
-            <pointLight intensity={7} position={[0, 1, 2]} />
-            <GdscLogo />
-        </Canvas>
+                <pointLight intensity={7} position={[0.5, 1, 2]} />
+                <pointLight intensity={7} position={[-0.5, 1, -2]} />
+                <GdscLogo />
+            </Canvas>
+            <Canvas>
+                <OrbitControls />
+                <ambientLight intensity={2} />
+                <pointLight intensity={7} position={[0.5, 1, 2]} />
+                <Bmo /> 
+            </Canvas>
+
         </>
     );
 }
