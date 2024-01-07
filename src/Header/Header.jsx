e735b52c088a07dc81fd392f89df37f05b7452f9
Jude <judeputhussery360@gmail.com>
2024-01-07 16:53:49 +0530

    Restructure Header
    individual responsive components

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,27 +1,60 @@
 import { Canvas } from "@react-three/fiber";
 import NavBar from "./NavBar";
-import { GdscLogo } from "../../components/GdscLogo";
-import { Bmo } from "../../components/Bmo";
+import { GdscLogo } from "../../components/3D/GdscLogo";
+import { Bmo } from "../../components/3D/Bmo";
 import { OrbitControls } from "@react-three/drei";
+import { Mouse } from "../../components/3D/Mouse";
+import { Box, Flex } from "@radix-ui/themes";
 const Header = () => {
-    return (
-        <>
-        <NavBar />
-        <Canvas>
-            <ambientLight intensity={2} />
-                <pointLight intensity={7} position={[0.5, 1, 2]} />
-                <pointLight intensity={7} position={[-0.5, 1, -2]} />
-                <GdscLogo />
-            </Canvas>
-            <Canvas>
-                <OrbitControls />
-                <ambientLight intensity={2} />
-                <pointLight intensity={7} position={[0.5, 1, 2]} />
-                <Bmo /> 
-            </Canvas>
-
-        </>
-    );
-}
+  return (
+    <>
+      <NavBar />
+      <Canvas>
+        <OrbitControls autoRotate autoRotateSpeed={8} />
+        <GdscLogo />
+      </Canvas>
+      <Flex
+        align={"center"}
+        justify={"center"}
+        wrap
+        style={{
+          padding: "1rem",
+          flexWrap: "wrap",
+          flexDirection: "row",
+        }}
+      >
+        <Box
+          style={{
+            minWidth: "400px",
+            flex: 1,
+            margin: "1rem",
+            backgroundColor: "var(--gray-a2)",
+            border: "2px solid var(--gray-a10)",
+            borderRadius: "var(--radius-5)",
+          }}
+        >
+          <Canvas>
+            <OrbitControls maxZoom={2} autoRotate autoRotateSpeed={8} />
+            <Bmo />
+          </Canvas>
+        </Box>
+        <Box
+          style={{
+            minWidth: "300px",
+            flex: 1,
+            margin: "1rem",
+            backgroundColor: "var(--gray-a2)",
+            border: "2px solid var(--gray-a10)",
+            borderRadius: "var(--radius-5)",
+          }}
+        >
+          <Canvas>
+            <OrbitControls maxZoom={2} autoRotate autoRotateSpeed={4} />
+            <Mouse />
+          </Canvas>
+        </Box>
+      </Flex>
+    </>
+  );
+};
 export default Header;
-    
