7fab333d6ff93e07fb55f892d0893034b7d16e77
Jude <judeputhussery360@gmail.com>
2024-01-18 16:39:34 +0530

    navbar edit

--- src/Header/NavBar.jsx
+++ src/Header/NavBar.jsx
@@ -1,26 +1,60 @@
-import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import "./style.css";
 const NavBar = () => {
   return (
-    <NavigationMenu.Root className="NavigationMenuRoot">
-      <NavigationMenu.List className="NavigationMenuList">
-        <NavigationMenu.Item className="NavigationMenuItem">
-          <NavigationMenu.Link className="NavigationMenuLink" href="#">
-            Home
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item className="NavigationMenuItem">
-          <NavigationMenu.Link className="NavigationMenuLink" href="#">
-            About
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-        <NavigationMenu.Item className="NavigationMenuItem">
-          <NavigationMenu.Link className="NavigationMenuLink" href="#">
-            Contact
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
-    </NavigationMenu.Root>
+    <header class="header active" data-header>
+      <div class="container">
+        <a href="#" class="logo">
+          <img
+            src="../assets/logo.png"
+            width="110"
+            height="53"
+            alt="zerolatency"
+          />
+        </a>
+
+        <nav class="navbar" data-navbar>
+          <ul class="navbar-list">
+            <li class="navbar-item">
+              <a href="#home" class="navbar-link" data-nav-link>
+                home
+              </a>
+            </li>
+
+            <li class="navbar-item">
+              <a href="#tournament" class="navbar-link" data-nav-link>
+                events
+              </a>
+            </li>
+
+            <li class="navbar-item">
+              <a href="#news" class="navbar-link" data-nav-link>
+                sponsors
+              </a>
+            </li>
+
+            <li class="navbar-item">
+              <a href="#" class="navbar-link" data-nav-link>
+                contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <a href="https://forms.gle/4hJcWo5N8dcaTPgX9" class="btn" data-btn>
+          Register Now
+        </a>
+
+        <button
+          class="nav-toggle-btn"
+          aria-label="toggle menu"
+          data-nav-toggler
+        >
+          <span class="line line-1"></span>
+          <span class="line line-2"></span>
+          <span class="line line-3"></span>
+        </button>
+      </div>
+    </header>
   );
 };
 export default NavBar;
